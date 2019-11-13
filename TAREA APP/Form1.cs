b4ebaa3e938ef40f2c9d8204dffd8b91aacc7cb4
marcos0801 <angulomarcos97@gmail.com>
2019-11-13 00:29:24 -0500

    rama cuadrado

--- TAREA APP/Form1.cs
+++ TAREA APP/Form1.cs
@@ -10,11 +10,61 @@ using System.Windows.Forms;
 
 namespace TAREA_APP
 {
-    public partial class Form1 : Form
+    public partial class cuadrado_de_unnumero : Form
     {
-        public Form1()
+        public cuadrado()
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            private void button3_Click(object sender, EventArgs e)
+            {
+                int nu;
+                if (this.n.Text.Length == 0)
+                {
+                    MessageBox.Show("OJO, cajas de textos vacia...!!");
+                    this.n.Focus();
+                    return;
+                }
+
+            }
     }
-}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             this.n.Text = "";
+            this.r.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int x = 0;
+            x = nu
+                  return x;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int x = 0;
+            x = nu * nu;
+            return x;
+        }
+    }
